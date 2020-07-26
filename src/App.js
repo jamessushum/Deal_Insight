97b6b05055d6994276e56d8d7a39569dcfe35bf6
James Su-Shum <sushumjames@gmail.com>
2020-07-26 18:27:56 -0500

    Modified display of navbar links per user authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/navBar/NavBar';
 import AppViews from './components/appViews/AppViews';
 import './App.css';
 
 const App = () => {
+  const isAuthenticated = () => sessionStorage.getItem('credentials') !== null
+
+  const [hasUser, setHasUser] = useState(isAuthenticated())
+
+  const clearUser = () => {
+    sessionStorage.clear()
+    setHasUser(isAuthenticated())
+  }
 
   return (
     <>
-      <NavBar />
+      <NavBar hasUser={hasUser} clearUser={clearUser} />
       <AppViews />
     </>
   )
